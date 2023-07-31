6fbaaa9eeb6175082f61a9efa7072efb93cef5d9
Gülsüm Vural <gulsum-vural@yandex.com>
2023-07-31 15:46:46 +0300

    feat: using FlatList component

--- app/index.js
+++ app/index.js
@@ -1,18 +1,30 @@
 import { useState } from "react";
-import { StyleSheet, Text, View, ScrollView } from "react-native";
+import { StyleSheet, Text, View, ScrollView, FlatList } from "react-native";
 
 export default function Page() {
   const [people, setPeople] = useState([
-    { name: "shaun", key: "1" },
-    { name: "yoshi", key: "2" },
-    { name: "mario", key: "3" },
-    { name: "luigi", key: "4" },
-    { name: "peach", key: "5" },
-    { name: "toad", key: "6" },
-    { name: "bowser", key: "7" },
+    { name: "shaun", id: "1" },
+    { name: "yoshi", id: "2" },
+    { name: "mario", id: "3" },
+    { name: "luigi", id: "4" },
+    { name: "peach", id: "5" },
+    { name: "toad", id: "6" },
+    { name: "bowser", id: "7" },
   ]);
 
   return (
+    // same thing done using FlatList component
+
+    <View style={styles.container}>
+      <FlatList
+        numColumns={2}
+        keyExtractor={(item) => item.id}
+        data={people}
+        renderItem={({ item }) => <Text style={styles.item}>{item.name}</Text>}
+      />
+    </View>
+
+    /*
     <ScrollView>
       <View style={styles.container}>
         {people.map((item) => (
@@ -22,6 +34,7 @@ export default function Page() {
         ))}
       </View>
     </ScrollView>
+    */
   );
 }
 
@@ -39,5 +52,6 @@ const styles = StyleSheet.create({
     padding: 30,
     backgroundColor: "pink",
     fontSize: 24,
+    marginHorizontal: 10,
   },
 });
